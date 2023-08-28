0bc751dd5fafb95a52cafa95a191c492005af1ec
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2023-08-28 21:08:11 +0500

    Add files via upload

---
+++ Calculator App/calcBody.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class calcBody extends JFrame {
+
+    final int HEIGHT = 500;
+    final int WIDTH = 500;
+
+    public calcBody() {
+        this.setSize(WIDTH, HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+
+        JPanel upperBody = new JPanel();
+
+        JPanel lowerBody = new JPanel();
+        lowerBody.setSize(500, 400);
+        lowerBody.s;
+        JButton b1 = new JButton("1");
+        JButton b2 = new JButton("2");
+        JButton b3 = new JButton("3");
+        JButton b4 = new JButton("4");
+        JButton b5 = new JButton("5");
+        JButton b6 = new JButton("6");
+        JButton b7 = new JButton("7");
+        JButton b8 = new JButton("8");
+        JButton b9 = new JButton("9");
+
+        this.setVisible(true);
+    }
+}
